10f725b29ceeb31bc1176c7d01d013a6ec1a8ec6
ribeiro-dev <romilson.2r@gmail.com>
2023-01-29 14:04:00 -0300

    displaying variables in HTML

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const app = express() // instaciando o express
 app.set("view engine", "ejs") // define o ejs como view engine
 
 // cria a rota /
-app.get('/', (req, res) => {
-    res.render("principal/perfil") // usamos o método render quando temos uma view engine
+app.get('/:nome/:lang/:level?', (req, res) => {
+    let nome = req.params["nome"]
+    let lang = req.params["lang"]
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        level: level
+    }) // usamos o método render quando temos uma view engine
 })
 
 app.listen(8080, () => {
