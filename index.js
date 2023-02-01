1add9401e5ea517c40db66581c8899c5d1c2314f
ribeiro-dev <romilson.2r@gmail.com>
2023-02-01 20:48:32 -0300

    getting form data

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express") // importando o módulo
 const app = express() // instaciando o express
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs") // define o ejs como view engine
 app.use(express.static('public')) // define a pasta de arquivos estáticos
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false })) // configura o bodyparser
+app.use(bodyParser.json()) // permite com que seja possivel ler json enviado 
 
 // cria a rota /
 app.get('/', (req, res) => {
@@ -12,6 +17,13 @@ app.get('/', (req, res) => {
 
 app.get('/perguntar', (req, res) => {
     res.render("perguntar.ejs")
+})
+
+app.post('/salvarpergunta', (req, res) => {
+    // o nome é de acordo com o atributo name
+    const title = req.body.title
+    const description = req.body.description
+    res.send("Formulario recebido! Titulo: " + title + " " + " descricao " + description)
 })
 
 app.listen(8080, () => {
